beac38338cc3ec2e96d918cee24982c62652f645
ExtranoDev <jeomade19@gmail.com>
2023-04-28 15:52:04 +0000

    creates multiple procedure

--- 0x00-MySQL_Advanced/6-bonus.sql
+++ 0x00-MySQL_Advanced/6-bonus.sql
@@ -1,11 +1,10 @@
 -- script that creates a stored procedure AddBonus that adds a new correction for a student
 DROP PROCEDURE IF EXISTS AddBonus;
-DELIMETER $$
+DELIMITER $$
 CREATE PROCEDURE AddBonus(
     IN user_id INT,
     IN project_name VARCHAR(255),
-    IN score FLOAT
-)
+    IN score FLOAT)
 BEGIN
     DECLARE project_id INT;
     IF (SELECT COUNT(*) FROM projects WHERE name = project_name) = 0
@@ -13,7 +12,7 @@ BEGIN
         INSERT INTO projects (name) VALUES (project_name);
     END IF;
     SET project_id = (SELECT id FROM projects WHERE name = project_name LIMIT 1);
-    INSERT INTO corrections (user_id, project_id, score) VALUES(user_id, prohect_id, score);
+    INSERT INTO corrections (user_id, project_id, score) VALUES(user_id, project_id, score);
 END
 $$
 DELIMITER;
