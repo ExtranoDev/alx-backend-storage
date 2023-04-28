1531a580982a4a52e13b1c954162475107ba9e4d
ExtranoDev <jeomade19@gmail.com>
2023-04-28 15:08:49 +0000

    returns result using idex search

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,6 +1,6 @@
 -- SQL script that lists all bands with Glam rock as their main style
 -- and ranked by their longevity
 
-SELECT band_name AS band_name, IFNULL(split, 2023) - IFNULL(formed, 0) AS lifespan
+SELECT band_name AS band_name, IFNULL(split, 2020) - IFNULL(formed, 0) AS lifespan
 FROM metal_bands
 WHERE style LIKE '%Glam rock%';
